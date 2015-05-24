fa12f981d86bcf9902ca399550c1512a2a9d11d3
MattReisman <mattreisman@gmail.com>
2015-05-24 04:28:54 -0600

    user authentication

--- app/client/routes.js
+++ app/client/routes.js
@@ -1,3 +1,13 @@
+angular.module("devices").run(["$rootScope", "$state", function($rootScope, $state) {
+  $rootScope.$on("$stateChangeError", function(event, toState, toParams, fromState, fromParams, error) {
+    // We can catch the error thrown when the $requireUser promise is rejected
+    // and redirect the user back to the main page
+    if (error === "AUTH_REQUIRED") {
+      $state.go('devices');
+    }
+  });
+}]);
+
 angular.module("devices").config(['$urlRouterProvider', '$stateProvider', '$locationProvider',
   function($urlRouterProvider, $stateProvider, $locationProvider){
 
@@ -12,7 +22,12 @@ angular.module("devices").config(['$urlRouterProvider', '$stateProvider', '$loca
     .state('deviceDetails', {
       url: '/devices/:deviceId',
       templateUrl: 'client/devices/views/devices-details.ng.html',
-      controller: 'DeviceDetailsCtrl'
+      controller: 'DeviceDetailsCtrl',
+      resolve: {
+        "currentUser": ["$meteor", function($meteor){
+      return $meteor.requireUser();
+    }]
+      }
     });
 
     $urlRouterProvider.otherwise("/devices");
--- app/model/devices.js
+++ app/model/devices.js
@@ -1,1 +1,19 @@
 Devices = new Mongo.Collection("devices");
+
+Devices.allow({
+  insert: function(userId, device) {
+    return userId && device.owner === userId;
+  },
+  update: function(userId, device, fields, modifier) {
+    if (userId !== device.owner)
+      return false;
+
+    return true;
+  },
+  remove: function(userId, device) {
+    if(userId !== device.owner)
+      return false;
+
+    return true;
+  }
+});
