840981d039afe8e58bb9a6d6d5876513a460ae9a
MattReisman <mattreisman@gmail.com>
2015-05-27 03:23:34 -0600

    Add accounts-ui bootstrap

---
+++ app/server/devices.js
@@ -0,0 +1,13 @@
+Meteor.publish("devices", function() {
+  return Devices.find({
+        $or:[
+      {$and:[
+        {"public": true},
+        {"public": {$exists: true}}
+      ]},
+      {$and:[
+        {owner: this.userId},
+        {owner: {$exists: true}}
+      ]}
+    ]});
+});
